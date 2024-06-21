cf1cb5c01662c3b903a3d87bf509912134573411
zahar.kalosha <zaharkalosha@gmail.com>
2024-06-21 10:35:30 +0300

    Update entity of user

---
+++ src/main/java/com/example/postgresdemo/model/User.java
@@ -0,0 +1,60 @@
+package com.example.postgresdemo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Size(min = 1, max = 50)
+    @Column(columnDefinition = "firstname")
+    private String firstname;
+    @Size(min = 1, max = 50)
+    @Column(columnDefinition = "lastname")
+    private String lastname;
+    @Column(columnDefinition = "password")
+    private String password;
+
+    public User() {}
+
+    public User(Long id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
